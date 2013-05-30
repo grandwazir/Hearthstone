c652c770085b381c3e8b0b4605ee121719465748
James Richardson <me+github@james.richardson.name>
2013-05-30 04:12:18 +0100

    update pom fix relocation of utilities into another plugins name space

--- src/main/java/name/richardson/james/hearthstone/general/HomeCommand.java
+++ src/main/java/name/richardson/james/hearthstone/general/HomeCommand.java
@@ -9,29 +9,30 @@ import name.richardson.james.hearthstone.Hearthstone;
 
 public class HomeCommand implements CommandExecutor {
 
-  private final AbstractCommand teleport; 
-  private final AbstractCommand set;
-  
-  public HomeCommand(Hearthstone plugin, AbstractCommand teleport, AbstractCommand set) {
-    this.teleport = teleport;
-    this.set = set;
-  }
-  
-  public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-    if (args.length == 0) {
-      this.teleport.onCommand(sender, command, label, args);
-    } else if (args[0].equalsIgnoreCase(this.set.getName())) {
-      String[] arguments;
-      arguments = prepareArguments(args);
-      this.set.onCommand(sender, command, label, arguments);
-    } 
-    return true;
-  }
-  
-  private String[] prepareArguments(String[] args) {
-    String[] arguments = new String[args.length - 1];
-    System.arraycopy(args, 1, arguments, 0, args.length - 1);
-    return arguments;
-  }
-  
+	private final AbstractCommand teleport;
+	private final AbstractCommand set;
+
+	public HomeCommand(final Hearthstone plugin, final AbstractCommand teleport, final AbstractCommand set) {
+		this.teleport = teleport;
+		this.set = set;
+	}
+
+	public boolean onCommand(final CommandSender sender, final Command command, final String label, final String[] args) {
+		if (args.length == 0) {
+			this.teleport.onCommand(sender, command, label, args);
+		} else
+			if (args[0].equalsIgnoreCase(this.set.getName())) {
+				String[] arguments;
+				arguments = this.prepareArguments(args);
+				this.set.onCommand(sender, command, label, arguments);
+			}
+		return true;
+	}
+
+	private String[] prepareArguments(final String[] args) {
+		final String[] arguments = new String[args.length - 1];
+		System.arraycopy(args, 1, arguments, 0, args.length - 1);
+		return arguments;
+	}
+
 }
